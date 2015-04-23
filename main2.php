163e16f639f29eeb106052ac019986e5f2a9eb0e
Pamela21 <Pamela21@users.noreply.github.com>
2015-04-22 19:28:15 -0600

    Update main2.php

--- main2.php
+++ main2.php
@@ -6,7 +6,7 @@ include_once 'header.php';
 Welcome to CPS LTD
 
 CPS Ltd. is the electronic commerce company with headquarters based in Salt Lake City, UT. The company offers wide variety of products ranging from electronic gadgets to cosmetics.  
-It was founded by three college students, Chris, Pamela and Saud. The primary focus of these three students was to offer good quality products to the customers.  
+It was founded by three college students, Chris, Pamela and Saud. The primary focus of these three students was to offer good quality products to the customers at their convenience.  
 CPS is known for its niche in popular culture and diversity of its products. Our attention to detail, customer service values and commitment to doing it right the first time is how we earned our reputation. 
 
 <?php
